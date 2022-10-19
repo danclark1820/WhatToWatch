6e77a188c71b9f434503ae607ce0bc7a75e73ba0
Daniel Clark <danclark1820@gmail.com>
2022-10-19 11:42:44 -0400

    Add two categories

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @movies = TMDBService.movies
+    @popular_movies = TMDBService.movies("popularity")
+    @highest_rated_movies = TMDBService.movies("vote_average")
+    # @popular_shows = TMDBService.shows("popularity")
+    # @highest_rated_shows = TMDBService.shows("votes")
   end
 end
--- app/services/TMDBService.rb
+++ app/services/TMDBService.rb
@@ -6,14 +6,22 @@ module TMDBService
   end
 
   class << self
-    def movies
-      formatted_movie_url = URL + "/discover/movie?sort_by=popularity.desc&api_key=" + TOKEN
+    def movies(sort_by)
+      formatted_movie_url = URL + "/discover/movie?sort_by=#{sort_by}.desc&certification_country=US&certification.gte=PG-13&vote_count.gte=3000&api_key=" + TOKEN
+      response = Faraday.get(formatted_movie_url)
+      parsed_response = JSON.parse(response.body)["results"]
+    end
+
+    def shows(sort_by)
+      formatted_movie_url = URL + "/discover/tv?sort_by=#{sort_by}.desc&api_key=" + TOKEN
       response = Faraday.get(formatted_movie_url)
       parsed_response = JSON.parse(response.body)["results"]
     end
 
     def image_url(poster_path)
-      "https://www.themoviedb.org/t/p/w440_and_h660_face/" + poster_path
+      if poster_path
+        "https://www.themoviedb.org/t/p/w440_and_h660_face/" + poster_path
+      end
     end
   end
 end
